60f03a93b8fd29a0b22a5840a87693eb908015f9
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-06-14 14:31:45 +0600

    Update entriesToObject.ts

--- src/entriesToObject.ts
+++ src/entriesToObject.ts
@@ -3,3 +3,7 @@
 export type EntriesToObject<T extends [string, any][]> = {
   [K in T[number][0]]: Extract<T[number], [K, any]>[1]
 }
+
+export type EntriesToObject<T extends [string, any][]> = {
+  [K in T[number] as K[0]]: K[1]
+}
