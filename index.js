f1bf85730f0ef12dd47bf016df2bef1cd8886523
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-06-14 12:55:00 +0000

    M index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function writeFile({ name, body }) {
       .filter(Boolean)
       .join('\n\n')
   )
-  const markdown = markdownParts.join('\n\n\n<hr />\n\n')
+  const markdown = markdownParts.join('\n\n---\n<hr />\n\n')
   fs.writeFileSync(path.join(outputDir, name + '.md'), markdown)
 }
 
