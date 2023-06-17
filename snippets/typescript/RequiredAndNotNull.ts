dceba67a81ba8f18030ee0a22474c571b9bf0aa3
al-imam <alimam01828@gmail.com>
2023-06-17 18:34:03 +0600

    create utility type RequiredAndNotNull
      - RequiredAndNotNull name says what it does!
      -  RequiredAndNotNull take second type augment
      - if second argument is true then clean object recursively

---
+++ snippets/typescript/RequiredAndNotNull.ts
@@ -0,0 +1,13 @@
+import Prettify from "./Prettify";
+
+type RequiredAndNotNull<T extends object, R = false> = Prettify<
+  Required<{
+    [key in keyof T]: R extends false
+      ? Exclude<T[key], null | undefined>
+      : T[key] extends object
+      ? RequiredAndNotNull<T[key], true>
+      : Exclude<T[key], null | undefined>;
+  }>
+>;
+
+export default RequiredAndNotNull;
